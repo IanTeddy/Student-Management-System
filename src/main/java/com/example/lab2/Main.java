da3e9e8d4518c6ffa12fbf5be55a6b633cf1e841
IanTeddy <136382277+IanTeddy@users.noreply.github.com>
2024-01-24 17:10:52 -0500

    modify setTitle

--- src/main/java/com/example/lab2/Main.java
+++ src/main/java/com/example/lab2/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("student_view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Student Management Systemmmm");
+        stage.setTitle("Student Management System");
         stage.setScene(scene);
         stage.show();
     }
